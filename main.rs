7d16b88360b5fb958da428b0f521c948361c8504
Evian Llerena <evianllerena@gmail.com>
2026-09-17 11:58:54 -0400

    Update startup diagnostics version to 0.5.6

--- main.rs
+++ main.rs
@@ -11,7 +11,7 @@ fn startup_log(message: &str) {
 }
 
 fn main() {
-    startup_log("0.5.5 process entered main()");
+    startup_log("0.5.6 process entered main()");
     std::panic::set_hook(Box::new(|info| {
         startup_log(&format!("PANIC: {}", info));
     }));
