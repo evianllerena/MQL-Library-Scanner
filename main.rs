2175050aa8e79708387f1fee96500ce6517a07be
Evian Llerena <evianllerena@gmail.com>
2026-09-17 22:56:59 -0400

    Version native startup marker as 0.5.10

--- main.rs
+++ main.rs
@@ -11,7 +11,7 @@ fn startup_log(message: &str) {
 }
 
 fn main() {
-    startup_log("0.5.9 process entered main()");
+    startup_log("0.5.10 process entered main()");
     std::panic::set_hook(Box::new(|info| {
         startup_log(&format!("PANIC: {}", info));
     }));
