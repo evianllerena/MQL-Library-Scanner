a1166702d673b43ea77b6db6106fe676a693c625
Evian Llerena <evianllerena@gmail.com>
2026-09-18 14:48:58 +0000

    removal/ re-add

--- v54.js
+++
@@ -1,4 +0,0 @@
-const b=document.querySelector('.brand small');
-if(b && b.textContent!=='0.5.6 • Evidence Engine v4 + Isolated MT4/MT5 Preview'){
-  b.textContent='0.5.6 • Evidence Engine v4 + Isolated MT4/MT5 Preview';
-}
